caf7626c1448f1e30f98eceeaca78f213b58207f
JugalG <81069461+JugalG@users.noreply.github.com>
2024-04-26 06:38:40 +0530

    all codes

---
+++ DC_RicartAgrawala.py
@@ -0,0 +1,34 @@
+numProcesses = int(input("Enter number of processes: "))
+numCS = int(input("Enter Number of processes that want to enter CS: "))
+# timestamp: process
+processMap = {}
+timestamps = []
+for i in range(numCS):
+    process, timestamp = input(f"Enter the Process ID and Timestamp of process: ").split()
+    processMap[int(timestamp)] = int(process)
+    timestamps.append(int(timestamp))
+print()
+timestamps.sort()
+for time in timestamps:
+    processCS = processMap[time]
+    for i in range(numProcesses):
+        if(processCS != i):
+            print(f"Process {processCS} has requested Process {i}")
+    print()
+for time in timestamps:
+    processCS = processMap[time]
+    for i in range(numProcesses):
+        if(processCS != i):
+            print(
+                f"Process {i} has Accepted the request of process {processCS}")
+    print()
+    print(f'Process {processCS} has now entered the CS')
+    print(f'Process {processCS} has now exited the CS')
+    print()
+
+
+# Enter number of processes: 4
+# Enter Number of processes that want to enter CS: 2
+# Enter the Process ID and Timestamp of process: 0 5
+# Enter the Process ID and Timestamp of process: 1 3
+
